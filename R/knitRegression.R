002d04621396cde5f4200ba739c59d960f247c65
stla <laurent_step@yahoo.fr>
2016-08-30 16:24:46 +0200

    first commit

---
+++ R/knitRegression.R
@@ -0,0 +1,11 @@
+#' Knit regression
+#' @export
+knitRegression <- function(dat, conflevel="95", filetype="word_document"){
+  dat <- as.data.frame(dat)
+  conflevel <- as.numeric(conflevel)/100
+  out <- rmarkdown::render(system.file(package = "opencpuHello", "templates", "regression.Rmd"),
+                           output_dir = getwd(),
+                           output_format = filetype,
+                           params = list(set_title="Regression analysis"))
+  return(basename(out))
+}
