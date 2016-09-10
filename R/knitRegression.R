a86fcd87c478b82e44b5d6ef5454f00108daf857
stla <laurent_step@yahoo.fr>
2016-09-10 13:33:57 +0200

    correction find package

--- R/knitRegression.R
+++ R/knitRegression.R
@@ -1,9 +1,9 @@
 #' Knit regression
 #' @export
-knitRegression <- function(dat, conflevel=95, filetype="word_document"){
+knitRegression <- function(dat, conflevel, filetype){
   dat <- as.data.frame(dat)
   conflevel <- conflevel/100
-  out <- rmarkdown::render(system.file(package = "opencpuHello", "templates", "regression.Rmd"),
+  out <- rmarkdown::render(system.file(package = "ocpusimplereg", "templates", "regression.Rmd"),
                            output_dir = getwd(),
                            output_format = filetype,
                            params = list(set_title="Regression analysis"))
