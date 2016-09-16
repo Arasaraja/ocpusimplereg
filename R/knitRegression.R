029e68ef864d1606db2179b7274b966db847892c
stla <laurent_step@yahoo.fr>
2016-09-16 20:50:17 +0200

    intermediate dir

--- R/knitRegression.R
+++ R/knitRegression.R
@@ -5,7 +5,8 @@ knitRegression <- function(dat, conflevel, filetype){
   conflevel <- conflevel/100
   out <- rmarkdown::render(system.file(package = "ocpusimplereg", "templates", "regression.Rmd"),
                            output_dir = getwd(),
-                           intermediates_dir = getwd(),
+                           intermediates_dir = if(filetype=="pdf_document") NULL else getwd(),
+                           runtime = "static",
                            output_format = filetype,
                            params = list(set_title="Regression analysis"))
   return(basename(out))
