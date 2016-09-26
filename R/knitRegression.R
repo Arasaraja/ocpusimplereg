b49ab1a61d56f7bc400961c56d7af84b3c806ac9
stla <laurent_step@yahoo.fr>
2016-09-26 13:33:22 +0200

    absolute path child

--- R/knitRegression.R
+++ R/knitRegression.R
@@ -3,9 +3,12 @@
 knitRegression <- function(dat, conflevel, filetype){
   dat <- as.data.frame(dat)
   conflevel <- conflevel/100
+  if(filetype=="word_document"){
+    filetype <- word_document(reference_docx = system.file(package = "ocpusimplereg", "templates", "word-styles-reference.docx"))
+  }
   out <- rmarkdown::render(system.file(package = "ocpusimplereg", "templates", "regression.Rmd"),
                            output_dir = getwd(),
-                           intermediates_dir = if(filetype=="pdf_document") NULL else getwd(),
+                           intermediates_dir = getwd(),
                            runtime = "static",
                            output_format = filetype,
                            params = list(set_title="Regression analysis"))
